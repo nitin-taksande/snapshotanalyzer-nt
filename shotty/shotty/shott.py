336ef72ba7d2929a6819c2334972eb55eadbc728
Nitin Taksande <NitinTaksande@corp.fairisaac.com>
2019-01-29 14:52:33 -0500

    added click module to the script

--- shotty/shotty/shott.py
+++ shotty/shotty/shott.py
@@ -1,8 +1,23 @@
 import boto3
+import click
+
+#This is making sure that the script always have a session and an ec2 resource
+session = boto3.Session(profile_name='nitin')
+ec2 = session.resource('ec2')
+
+@click.command()
+#function to give list of EC2 instances
+def list_instances():
+        "List EC2 Instances"
+        for i in ec2.instances.all():
+            print(', ' .join((
+                i.id,
+                i.instance_type,
+                i.placement['AvailabilityZone'],
+                i.state['Name'],
+                i.public_dns_name)))
+        return
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='nitin')
-    ec2 = session.resource('ec2')
-    for i in ec2.instances.all():
-        print(i)
-input()
+    list_instances()
+    input()
